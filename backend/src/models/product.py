88c00f73892d8b94cf85557f7b90a552358f6484
Albert <thegoversus@gmail.com>
2024-03-14 02:59:55 +0300

    product model

---
+++ backend/src/models/product.py
@@ -0,0 +1,20 @@
+"""
+Product models
+"""
+from decimal import Decimal
+
+from sqlmodel import SQLModel, Field
+
+
+class Product(SQLModel, table=True):
+    """Product model"""
+    id: int = Field(primary_key=True, index=True)
+    name: str = Field()
+    price: Decimal = Field()
+    sale_price: Decimal | None = Field(default=None)
+    category: str | None = Field(nullable=True)
+    subcategory: str | None = Field(nullable=True)
+    brand: str | None = Field(nullable=True)
+    unit: str | None = Field(default="шт", nullable=True)
+    producer: str | None = Field(nullable=True)
+    producer_country: str | None = Field(nullable=True)
