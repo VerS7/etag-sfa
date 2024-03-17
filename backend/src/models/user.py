8cc53a3636bc2c2e25072f9838317cbc704c580d
Albert <thegoversus@gmail.com>
2024-03-17 05:18:33 +0300

    role field

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -8,3 +8,4 @@ class User(SQLModel, table=True):
     id: int = Field(primary_key=True, index=True)
     username: str = Field(nullable=False)
     hashed_password: str = Field(nullable=False)
+    role: str = Field(nullable=False, default="user")
