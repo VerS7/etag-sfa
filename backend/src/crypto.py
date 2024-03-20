b85c80d8268332336b97306938ff215bc63f33f9
Albert <thegoversus@gmail.com>
2024-03-21 02:24:28 +0300

    move hash alg outside from hash_password

--- backend/src/crypto.py
+++ backend/src/crypto.py
@@ -4,8 +4,15 @@ Cryptography
 import hashlib
 
 
+def sha256_hash_function(to_hash: str) -> str:
+    method = hashlib.sha256()
+    method.update(to_hash.encode("utf-8"))
+    return method.hexdigest()
+
+
+hash_func = sha256_hash_function
+
+
 def hash_password(password: str) -> str:
     """Returns hashed password"""
-    method = hashlib.sha256()
-    method.update(password.encode("utf-8"))
-    return method.hexdigest()
+    return hash_func(password)
