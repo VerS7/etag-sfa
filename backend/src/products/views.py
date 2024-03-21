374c5b9576906ee0da8a9017164c15ff87fcf99f
Albert <thegoversus@gmail.com>
2024-03-21 04:09:34 +0300

    access to endpoints via token or user auth

--- backend/src/products/views.py
+++ backend/src/products/views.py
@@ -5,6 +5,13 @@ from fastapi import APIRouter, HTTPException, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.src.database.db import get_session
+
+from backend.src.token_auth.security import token_auth
+from backend.src.user_auth.security import auth_credentials
+
+from backend.src.models.token import AuthToken
+from backend.src.models.user import User
+
 from . import crud
 from .schemas import Product, ProductCreate, ProductUpdatePartial
 
@@ -13,13 +20,33 @@ router = APIRouter(prefix="/product", tags=["Products"])
 
 
 @router.get("/", response_model=list[Product])
-async def get_products(session: AsyncSession = Depends(get_session)):
+async def get_products(session: AsyncSession = Depends(get_session),
+                       user_access: User = Depends(auth_credentials)):
     """get all products endpoint"""
     return await crud.get_products(session)
 
 
 @router.get("/{product_id}/", response_model=Product)
-async def get_product(product_id: int, session: AsyncSession = Depends(get_session)):
+async def get_product(product_id: int, session: AsyncSession = Depends(get_session),
+                      user_access: User = Depends(auth_credentials)):
+    """get products by id endpoint"""
+    product = await crud.get_product(session, product_id)
+    if product is not None:
+        return product
+    raise HTTPException(status_code=404, detail="Item not found")
+
+
+@router.get("/{token}", response_model=list[Product])
+async def get_products(session: AsyncSession = Depends(get_session),
+                       token_access: AuthToken = Depends(token_auth)):
+    """get all products endpoint"""
+    return await crud.get_products(session)
+
+
+@router.get("/{token}/{product_id}/", response_model=Product)
+async def get_product(product_id: int,
+                      session: AsyncSession = Depends(get_session),
+                      token_access: AuthToken = Depends(token_auth)):
     """get products by id endpoint"""
     product = await crud.get_product(session, product_id)
     if product is not None:
@@ -28,7 +55,8 @@ async def get_product(product_id: int, session: AsyncSession = Depends(get_sessi
 
 
 @router.post("/", response_model=Product, status_code=status.HTTP_201_CREATED)
-async def create_product(product: ProductCreate, session: AsyncSession = Depends(get_session)):
+async def create_product(product: ProductCreate, session: AsyncSession = Depends(get_session),
+                         user_access: User = Depends(auth_credentials)):
     """create product endpoint"""
     return await crud.add_product(session, product)
 
@@ -36,8 +64,8 @@ async def create_product(product: ProductCreate, session: AsyncSession = Depends
 @router.put("/{product_id}/")
 async def update_product_partial(product_id: int,
                                  product_update: ProductUpdatePartial,
-                                 session: AsyncSession = Depends(get_session)
-                                 ):
+                                 session: AsyncSession = Depends(get_session),
+                                 user_access: User = Depends(auth_credentials)):
     """update product endpoint"""
     product = await crud.get_product(session, product_id)
     if product is not None:
@@ -52,8 +80,8 @@ async def update_product_partial(product_id: int,
 
 @router.delete("/{product_id}/", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_product(product_id: int,
-                         session: AsyncSession = Depends(get_session)
-                         ):
+                         session: AsyncSession = Depends(get_session),
+                         user_access: User = Depends(auth_credentials)):
     """delete product endpoint"""
     product = await crud.get_product(session, product_id)
     if product is not None:
