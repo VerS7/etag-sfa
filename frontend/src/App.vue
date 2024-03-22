69709faa60b98f9802a81449d39840b48f0ab6db
Albert <thegoversus@gmail.com>
2024-03-22 04:46:04 +0300

    frontend app template

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div class="greetings">Пока тут ничего нет...</div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+}
+.greetings {
+  margin: auto;
+  font-size: 55px;
+}
+</style>
