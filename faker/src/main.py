4e4139bf568d3ad84eb7ab4590201e1faf09b5a3
Albert <thegoversus@gmail.com>
2024-04-10 02:44:12 +0300

    faker main script

---
+++ faker/src/main.py
@@ -0,0 +1,43 @@
+"""
+Simple script for adding mock data to app API
+"""
+import os.path
+from random import choice, randint
+from os import getenv, path
+
+import requests
+from dotenv import load_dotenv
+
+from fakegen.loader import load_all
+from fakegen.generator import ProductBuilder
+
+
+load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+
+data_files = os.path.join(os.path.dirname(__file__), "fakegen/categories")
+api_url = f"{getenv('API_URL')}:{getenv('API_PORT')}/api/product/{getenv('API_TOKEN')}"
+
+random_barcodes = [str(randint(111_111_111, 999_999_999)) for _ in range(150)]
+
+builder = ProductBuilder(
+    data=load_all(data_files),
+    barcode_generator=lambda: choice(random_barcodes),
+    price_multiplier=1.3,
+    sale_chance=0.4,
+    sale_multiplier=0.2
+)
+
+with requests.Session() as session:
+    while True:
+        try:
+            product = list(builder.generate_completed_product())[0]
+            print(f"POST | {str(product)[:70]}... | ", end="")
+            response = requests.post(api_url, data=product.model_dump_json().encode("utf-8"))
+            print("SUCCESS!")
+
+        except requests.exceptions.ConnectionError:
+            print(f"FAIL!\n")
+            exit()
+
+        except StopIteration:
+            exit()
